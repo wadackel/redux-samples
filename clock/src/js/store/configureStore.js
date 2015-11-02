3c6f22da70c78de9c8220b9e9e39206c8ac245d1
tsuyoshiwada <v23.b.wada@gmail.com>
2015-11-03 02:31:10 +0900

    Update remove redux-thunk

--- clock/src/js/store/configureStore.js
+++ clock/src/js/store/configureStore.js
@@ -1,10 +1,8 @@
 import {createStore, applyMiddleware} from "redux"
-import thunk from "redux-thunk"
 import logger from "redux-logger"
 import reducer from "../reducers/"
 
 const createStoreWithMiddleware = applyMiddleware(
-  thunk,
   logger()
 )(createStore);
 
